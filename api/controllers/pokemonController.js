c6daf6526eb4775f5845b550142739626d2f3cd9
Nicolas Quillon <nicolas.quillon@imhotepcreation.com>
2017-10-23 14:00:33 +0200

    ajout bcrypt

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -2,65 +2,66 @@
 var https = require('https');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'pokecard'
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'pokecard'
 });
 
 
 connection.connect();
 
-exports.test = function(req, res){
-	res.json({test: "test"});
+exports.test = function(req, res) {
+	res.json({ test: "test" });
 }
 
-exports.pokedex = function(req, res){
+exports.pokedex = function(req, res) {
 
 	var options = "https://pokeapi.co/api/v2/pokedex/1/";
 
 	var data = "";
 
 	var request = https.get(options, (result) => {
-	  	result.on('data', (d) => {
-	  	 	data += d;
-	  	});
-	  	result.on('end', function(){
-	  		res.json(JSON.parse(data));
-	  	});
+		result.on('data', (d) => {
+			data += d;
+		});
+		result.on('end', function() {
+			res.json(JSON.parse(data));
+		});
 	});
 
 	request.on('error', (e) => {
-	  console.error(e);
+		console.error(e);
 	});
 
 	request.end();
 }
 
-exports.init = function(req, res){
+exports.init = function(req, res) {
 	var userId = req.params.userId;
 
 	connection.connect();
 
 	var response = {};
- 
-	connection.query('SELECT * FROM User WHERE idUser =' + userId, function (error, results, fields) {
-	  	response = results[0];
-	  	
+
+	connection.query('SELECT * FROM User WHERE idUser =' + userId, function(error, results, fields) {
+		response = results[0];
+
 		res.json(response);
 	});
 }
 
-exports.login = function(req, res){
+exports.login = function(req, res) {
 	var mail = req.body.mail;
 
 	var response = {};
- 
-	connection.query('SELECT * FROM User WHERE mail LIKE "' + mail + '"', function (error, results, fields) {
-	  	if(results){
-	  		res.json(results[0]);
-	  	}else{
-	  		res.json({user: false});
-	  	}
+
+	connection.query('SELECT * FROM User WHERE mail LIKE "' + mail + '"', function(error, results, fields) {
+		if(results) {
+			if(results[0].password)
+				res.json(results[0]);
+		} else {
+			res.json({ user: false });
+		}
 	});
 }
