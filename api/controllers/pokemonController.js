80c159cdfa542895c750ffa6446322d49fe0d9a2
Nicolas <nicolas.quillon@etu.univ-lyon1.fr>
2017-11-10 17:27:00 +0100

    pokemon/1

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -3,14 +3,18 @@ var https = require('https');
 
 exports.pokedex = function(req, res) {
 
+	console.log("/pokedex")
+
 	var options = "https://pokeapi.co/api/v2/pokemon/?limit=802&offset=0";
 
 	var data = "";
 
 	var request = https.get(options, (result) => {
+
 		result.on('data', (d) => {
 			data += d;
 		});
+
 		result.on('end', function() {
 			var tmpData = JSON.parse(data);
 
@@ -25,6 +29,7 @@ exports.pokedex = function(req, res) {
 			}
 			res.json(finalResult);
 		});
+
 	});
 
 	request.on('error', (e) => {
@@ -35,5 +40,40 @@ exports.pokedex = function(req, res) {
 }
 
 exports.pokemonDetails = function(req, res){
-	
+
+	var idPokemon = req.params.idPokemon;
+
+	console.log("/pokemon/" + idPokemon);
+
+	var options = "https://pokeapi.co/api/v2/pokemon/?limit=802&offset=0";
+	var data = "";
+	var request = https.get(options, (result) => {
+
+		result.on('data', (d) => {
+			data += d;
+		});
+
+		result.on('end', function() {
+			console.log(data)
+			var tmpData = JSON.parse(data);
+
+			//var finalResult = {"pokedex": []};
+
+			/*for(var i=1; i<=802; i++){
+				finalResult.pokedex.push({
+					"id": i,
+					"name": tmpData.results[i-1].name,
+					"urlPicture": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + i + ".png"
+				});
+			}*/
+			res.json(tmpData);
+		});
+
+	});
+
+	request.on('error', (e) => {
+		console.error(e);
+	});
+
+	request.end();
 }
