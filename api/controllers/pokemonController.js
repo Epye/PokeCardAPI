167010976490aeb143a62d60e5a75ea7885307f3
Nicolas <nicolas.quillon@etu.univ-lyon1.fr>
2017-11-14 14:26:44 +0100

    Récupération des cartes et de détail d'un pokemon

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -45,7 +45,7 @@ exports.pokemonDetails = function(req, res){
 
 	console.log("/pokemon/" + idPokemon);
 
-	var options = "https://pokeapi.co/api/v2/pokemon/?limit=802&offset=0";
+	var options = "https://pokeapi.co/api/v2/pokemon/" + idPokemon + "/";
 	var data = "";
 	var request = https.get(options, (result) => {
 
@@ -54,21 +54,43 @@ exports.pokemonDetails = function(req, res){
 		});
 
 		result.on('end', function() {
-			console.log(data)
 			var tmpData = JSON.parse(data);
+			var resultData = {
+				"id": tmpData.id,
+				"name": tmpData.name,
+				"weight": tmpData.weight,
+				"height": tmpData.height,
+				"urlPicture": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + idPokemon + ".png",
+				"cards": []
+			}	
 
-			//var finalResult = {"pokedex": []};
+			data = "";
 
-			/*for(var i=1; i<=802; i++){
-				finalResult.pokedex.push({
-					"id": i,
-					"name": tmpData.results[i-1].name,
-					"urlPicture": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + i + ".png"
+			var request2 = https.get("https://api.pokemontcg.io/v1/cards?name=" + tmpData.name, (results) => {
+				
+				results.on('data', (d) => {
+					data += d;
 				});
-			}*/
-			res.json(tmpData);
-		});
 
+				results.on('end', function() {
+					var tmpCard = JSON.parse(data);
+					tmpCard.cards.forEach(function(card){
+						resultData.cards.push({
+							"id": card.id,
+							"urlPicture": card.imageUrlHiRes
+						})
+					})
+					res.json(resultData);
+				});
+
+			});
+
+			request2.on('error', (e) => {
+				console.error(e);
+			});
+
+			request.end();
+		});
 	});
 
 	request.on('error', (e) => {
